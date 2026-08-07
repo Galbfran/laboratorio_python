3d6d6ac78816995f89dd9a4ce5bf2694a923a18b
Galbfran <francogalbiati984@gmail.com>
2026-08-07 15:58:30 -0300

    Agregar lógica para imprimir autores de libros y libros por autor; mejorar formato de salida

--- segundo_ejercicio/src/segundo_ejercicio/__init__.py
+++ segundo_ejercicio/src/segundo_ejercicio/__init__.py
@@ -1,6 +1,9 @@
 def main() -> None:
-    print("Valor total de los libros: $" + str(get_total_price(libros)))
-    print("Libros sin stock:" + str(get_books_not_stock(libros)))
+    print(f"Valor total de los libros: ${get_total_price(libros)}")
+    print(f"Libros sin stock: {get_books_not_stock(libros)}")
+    print(f"Autores de los libros: {get_authos(libros)}")
+    print(f"Libros de Robert Martin: {get_books_by_author(libros, 'Robert Martin')}")
+    print("Fin del programa.")
 
 libros = [
     {"titulo": "Clean Code", "autor": "Robert Martin", "precio": 25000, "stock": 3},
@@ -22,11 +25,21 @@ def get_total_price(books: list[dict[str, str| int]]) -> float:
                 print(f"El stock del libro '{book['titulo']}' no es un número válido.")
     return total
 
-def get_books_not_stock(books: list[dict[str, str| int]]) -> list[str]:
+def get_books_not_stock(books: list[dict[str, str | int]]) -> list[str]:
     not_in_stock = []
     for book in books:
         if isinstance(book["stock"], int) and book["stock"] == 0:
             not_in_stock.append(book["titulo"])
-        else:
-            print(f"El libro '{book['titulo']}' no tiene stock disponible.")
     return not_in_stock
+
+def get_authos(books: list[dict[str, str| int]]) -> set[str]:
+    books_by_author = set(book["autor"] for book in books if isinstance(book["autor"], str))
+   
+    return books_by_author
+
+def get_books_by_author(books: list[dict[str, str| int]], author: str) -> list[str]:
+    books_by_author = []
+    for book in books:
+        if isinstance(book["autor"], str) and book["autor"] == author:
+            books_by_author.append(book["titulo"])
+    return books_by_author
